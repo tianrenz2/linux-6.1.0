03ce44893ae77f37aba49b7d6584c1fee3c82372
Silver Zhang <silvertianren@gmail.com>
2024-04-22 10:51:35 -0400

    Keep updating when queue is full

--- arch/x86/kernel/kvm_ivshmem.c
+++ arch/x86/kernel/kvm_ivshmem.c
@@ -455,6 +455,8 @@ rr_event_log_guest *rr_alloc_new_event_entry(void)
     if (header->current_pos == header->total_pos - 1) {
         printk(KERN_ERR "RR queue is full\n");
 		header->current_pos = 0;
+		//return NULL;
+
     }
 
     entry = (rr_event_log_guest *)(kvm_ivshmem_dev.base_addr + \
